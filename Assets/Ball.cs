b247b4b42f4b2811af2f01253f10691a04dc2460
olimjonolimov7 <115147970+olimjonolimov7@users.noreply.github.com>
2024-09-20 16:25:09 +0500

    Add files via upload

---
+++ Assets/Ball.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float startingSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool isRight=UnityEngine.Random.value >=0.5;
+        float xVelocity=-1f;
+        if(isRight==true)
+        {
+            xVelocity=1f;
+        }
+        float yVelocity=UnityEngine.Random.Range(-1, 1);
+        rb.velocity=new Vector2(xVelocity*startingSpeed, yVelocity*startingSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/P2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2 : MonoBehaviour
+{
+    
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isPressingUp=Input.GetKey(KeyCode.UpArrow);
+        bool isPressingDown=Input.GetKey(KeyCode.DownArrow);
+        if(isPressingUp)
+        {
+            transform.Translate(Vector2.up*Time.deltaTime*moveSpeed);
+        }
+        if(isPressingDown)
+        {
+            transform.Translate(Vector2.down*Time.deltaTime*moveSpeed);
+        }
+    }
+}
